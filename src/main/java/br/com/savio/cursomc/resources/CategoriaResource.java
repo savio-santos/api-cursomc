f2d96984d6e4967982ed3b6900ff937ffcd69233
Sávio Santos <saviosantos276@gmail.com>
2020-09-02 01:49:53 -0300

    testando classe de dominio categoria

--- src/main/java/br/com/savio/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/savio/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,27 @@
 package br.com.savio.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.savio.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	 @GetMapping
-	 public String listar() {
-		 return "teste rest";
+	 public List<Categoria> listar() {
+		 Categoria cat1 = new Categoria(1, "informatica");
+		 Categoria cat2 = new Categoria(2, "escrritorio");
+		 
+		 List<Categoria> lista = new ArrayList<>();
+		 lista.add(cat1);
+		 lista.add(cat2);
+		 return lista;
 	 }
 
 }
