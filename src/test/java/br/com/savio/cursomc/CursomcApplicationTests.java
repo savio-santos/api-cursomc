50cdd5409a3094bd67315c626d070bb88c139819
Sávio Santos <saviosantos276@gmail.com>
2020-09-02 00:46:03 -0300

    Projeto criado

---
+++ src/test/java/br/com/savio/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.savio.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
