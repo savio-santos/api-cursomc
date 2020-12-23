5a3ab5957ed0826c53099f934bd37a0a78eef65e
Sávio Santos <saviosantos276@gmail.com>
2020-12-23 19:25:56 -0300

    criando profile de desenvolvimento

---
+++ src/main/java/br/com/savio/cursomc/config/DevConfig.java
@@ -0,0 +1,33 @@
+package br.com.savio.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.savio.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatebase() throws ParseException {
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+
+			dbService.instantiateTestDatabase();
+		
+		return true;
+	}
+}
